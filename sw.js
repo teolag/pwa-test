de8d7ead913c90c655a93c28c26e17fe22e4e4df
Teodor Lagerqvist <git@teodor.se>
2024-09-27 19:42:01 +0200

    Initial code fron chat-gpt

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'simple-pwa-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+// Install event - caching files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch event - serving files from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
