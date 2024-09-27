5fb0266c3756a60e8d2af8bb709671da4994d77c
Teodor Lagerqvist <git@teodor.se>
2024-09-27 20:07:34 +0200

    Add icon-files

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-const cacheName = 'simple-pwa-v1';
-const assetsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
-];
+const cacheName = 'simple-pwa-v1'
+const assetsToCache = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png']
 
 // Install event - caching files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
-      return cache.addAll(assetsToCache);
+      return cache.addAll(assetsToCache)
     })
-  );
-});
+  )
+})
 
 // Fetch event - serving files from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request)
     })
-  );
-});
+  )
+})
